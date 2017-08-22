bbeb77db763954119559a43967bc6b79229729a2
Evan.W <evan.wang@yesee.com.tw>
2017-08-22 16:15:44 +0800

    ###################### # 解决java产生文件 ###################### *.class
    # Mobile Tools for Java (J2ME)
    .mtj.tmp/
    
    # Package Files #
    *.jar
    *.war
    *.ear
    
    # virtual machine crash logs, see http://www.java.com/en/download/help/error_hotspot.xml
    hs_err_pid*
    
    ######################
    # 解决maven产生的文件
    ######################
    
    target/
    **/target/
    pom.xml.tag
    pom.xml.releaseBackup
    pom.xml.versionsBackup
    pom.xml.next
    release.properties
    dependency-reduced-pom.xml
    buildNumber.properties
    .mvn/timing.properties
    
    ######################
    # 解决各类编辑器自动产生的文件
    ######################
    
    *.iml
    
    ## Directory-based project format:
    .idea/
    # if you remove the above rule, at least ignore the following:
    
    # User-specific stuff:
    # .idea/workspace.xml
    # .idea/tasks.xml
    # .idea/dictionaries
    
    # Sensitive or high-churn files:
    # .idea/dataSources.ids
    # .idea/dataSources.xml
    # .idea/sqlDataSources.xml
    # .idea/dynamic.xml
    # .idea/uiDesigner.xml
    
    # Gradle:
    # .idea/gradle.xml
    # .idea/libraries
    
    # Mongo Explorer plugin:
    # .idea/mongoSettings.xml
    
    ## File-based project format:
    *.ipr
    *.iws
    
    ## Plugin-specific files:
    
    # IntelliJ
    /out/
    /target/
    
    # mpeltonen/sbt-idea plugin
    .idea_modules/
    
    # JIRA plugin
    atlassian-ide-plugin.xml
    
    # Crashlytics plugin (for Android Studio and IntelliJ)
    com_crashlytics_export_strings.xml
    crashlytics.properties
    crashlytics-build.properties

--- data_structure/src/test/java/test/data_structure/tree/TestTree.java
+++ data_structure/src/test/java/test/data_structure/tree/TestTree.java
@@ -2,10 +2,12 @@ package test.data_structure.tree;
 
 import org.junit.Test;
 
+import com.google.common.base.Strings;
+
 public class TestTree {
 	
 	@Test
 	public void testTree() {
-		
+		Strings.isNullOrEmpty("");
 	}
 }
